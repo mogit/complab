891cf1c13a36af217d026fca8d63c433e18a19fc
mohit <mohit.1093@gmail.com>
2012-10-14 20:01:34 +0530

    all done

--- OCTAVE/a05_1.m
+++ OCTAVE/a05_1.m
@@ -18,7 +18,45 @@ T=imread("input.bmp");
 	pre_y=55;
 	pre_k=k;
 
-	
+	for(y=155:100:755)
+		k=0;
+		for (x=1:600)
+			k=k+1;
+			if(T(x,y)==1)
+				break;
+			end
+		end
+		for(temp1=pre_k+20:pre_k+30)
+			for (temp2=pre_y:pre_y+55)
+				P(temp1,temp2)=0;
+			end
+		end
+
+		for(temp1=k+20:k+30)
+			for (temp2=y-55:y)
+				P(temp1,temp2)=0;
+			end
+		end
+
+		if(k+20>pre_k+30)
+			for(temp2=pre_k+30:k+20)
+				for(temp1=((pre_y+y)/2)-5:((pre_y+y)/2)+5)
+					P(temp2,temp1)=0;
+				end
+			end
+
+		else
+			for(temp2=k+20:pre_k+30)
+				for(temp1=((pre_y+y)/2)-5:((pre_y+y)/2)+5)
+					P(temp2,temp1)=0;
+				end
+			end
+		end
+
+		pre_y=y;
+		pre_k=k;
+	end
+
 		for (temp1= pre_k+20:pre_k+30)
 			for temp2= 745:800
 				P(temp1,temp2)=0;
